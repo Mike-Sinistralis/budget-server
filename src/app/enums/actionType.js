0bdca744b03a884b9b0799361312d4c98d1a6df6
mzimmerman <mzimmerman0205@gmail.com>
2016-01-01 22:04:44 -0500

    Quick commit

--- src/app/enums/actionType.js
+++
@@ -1,9 +0,0 @@
-import { Map } from 'immutable';
-
-const actionTypes = Map({
-  SET_ENTRIES:'setEntries',
-  NEXT:'next',
-  VOTE:'vote'
-});
-
-export { actionTypes };
---
+++ test.js
@@ -0,0 +1,21 @@
+import { connect } from 'rethinkdb';
+
+connect({
+	
+	}:, () => {
+
+	}
+});
+
+/**
+
+	ssl public key on dashboard
+	use to create own key
+	put that undser ssh folder
+	C:\Users\barric.reed\.ssh
+	name it id_rsa
+	compose io, create new user of type ssh
+	http login name
+	can now open ssh tunnel
+
+**/
