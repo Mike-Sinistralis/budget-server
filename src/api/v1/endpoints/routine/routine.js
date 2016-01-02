6746a23a5b1d0123c83b0edeb478a94f7585e56d
mzimmerman <mzimmerman0205@gmail.com>
2016-01-02 17:18:45 -0500

    Forgot to add BadRequestError. Desperately need to figure out how to write unit tests for this

--- src/api/v1/endpoints/routine/routine.js
+++ src/api/v1/endpoints/routine/routine.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { NotFoundError } from '../../../../utils/errors';
+import { NotFoundError, BadRequestError } from '../../../../utils/errors';
 
 function routineApi() 
 {
